deb6bc19b029ed128ba24683da7a1023a20e710d
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-19 10:03:13 -0300

    api: create error handling in app

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "express-async-errors";
+import { ErrorHandling } from "@middlewares/ErrorHandling";
 import { router } from "./routes";
 
 const app = express();
@@ -6,5 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(ErrorHandling);
 
 export { app };
