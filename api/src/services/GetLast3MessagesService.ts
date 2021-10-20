c537e6b0c563d49acb1cb45f5d72bf1e1e129710
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-20 10:21:05 -0300

    feat(api): create service get 3 last messages

---
+++ api/src/services/GetLast3MessagesService.ts
@@ -0,0 +1,19 @@
+import prismaClient from "@prisma/index";
+
+class GetLast3MessagesService {
+  async execute() {
+    const messages = await prismaClient.message.findFirst({
+      take: 3,
+      orderBy: {
+        created_at: "desc"
+      },
+      include: {
+        user: true
+      }
+    });
+
+    return messages;
+  }
+}
+
+export { GetLast3MessagesService };
