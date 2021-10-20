68eb9b9b4195e5cc800a7d7f3017045729c2eb15
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-20 08:25:22 -0300

    feat(api): adding messages route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+import { EnsureAuthenticated } from "@middlewares/EnsureAuthenticated";
+
 import { AuthenticateUserController } from "@controllers/AuthenticateUserController";
+import { CreateMessageController } from "@controllers/CreateMessageController";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+
+router.post("/messages", EnsureAuthenticated, new CreateMessageController().handle);
 
 export { router };
