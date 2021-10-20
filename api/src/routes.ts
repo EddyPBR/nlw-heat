10e1fa3dd0a17663b81d47c5bc4dfe7396581422
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-20 08:53:29 -0300

    feat(api): adding new route to authenticate with github & callback route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -7,8 +7,19 @@ import { CreateMessageController } from "@controllers/CreateMessageController";
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle);
+router.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
+router.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.status(200).json(code);
+});
+
+router.post("/authenticate", new AuthenticateUserController().handle);
 
 router.post("/messages", EnsureAuthenticated, new CreateMessageController().handle);
 
