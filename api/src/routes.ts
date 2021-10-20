dbbf76287117ce7c9cfa414c1867f01dcca12900
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-20 10:22:08 -0300

    feat(api): create route to get 3 last messages

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -4,6 +4,7 @@ import { EnsureAuthenticated } from "@middlewares/EnsureAuthenticated";
 
 import { AuthenticateUserController } from "@controllers/AuthenticateUserController";
 import { CreateMessageController } from "@controllers/CreateMessageController";
+import { GetLast3MessagesController } from "@controllers/GetLast3MessagesController";
 
 const router = Router();
 
@@ -22,5 +23,7 @@ router.get("/signin/callback", (request, response) => {
 router.post("/authenticate", new AuthenticateUserController().handle);
 
 router.post("/messages", EnsureAuthenticated, new CreateMessageController().handle);
+
+router.post("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
