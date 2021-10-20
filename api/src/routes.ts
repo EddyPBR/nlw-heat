ca48d5599e8222c82fc5590cbd468b8e9e3ec7e1
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-20 10:35:50 -0300

    feat(api): create route /profile

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -5,6 +5,7 @@ import { EnsureAuthenticated } from "@middlewares/EnsureAuthenticated";
 import { AuthenticateUserController } from "@controllers/AuthenticateUserController";
 import { CreateMessageController } from "@controllers/CreateMessageController";
 import { GetLast3MessagesController } from "@controllers/GetLast3MessagesController";
+import { ProfileUserController } from "@controllers/ProfileUserController";
 
 const router = Router();
 
@@ -25,5 +26,7 @@ router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", EnsureAuthenticated, new CreateMessageController().handle);
 
 router.post("/messages/last3", new GetLast3MessagesController().handle);
+
+router.post("/profile", EnsureAuthenticated, new ProfileUserController().handle);
 
 export { router };
