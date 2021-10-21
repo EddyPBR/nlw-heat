1089dd81e4adf959cde37b6f750746589a3d4b27
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-21 17:00:54 -0300

    feat(web): login box component integrated with authHook

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,12 +1,18 @@
 import { VscGithubInverted } from "react-icons/vsc";
 
+import { useAuth } from "@hooks/useAuth";
+
 import { Wrapper, SignInButton } from "./styles";
 
+
 export function LoginBox() {
+  const { signInUrl } = useAuth();
+
   return (
     <Wrapper>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <SignInButton>
+
+      <SignInButton href={signInUrl} >
         <VscGithubInverted size={24} />
         Entrar com github
       </SignInButton>
