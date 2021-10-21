143adf57d516f4f8f7b886a6d34d0ee2b28efba0
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-21 14:20:08 -0300

    feat(web): integate component MessagesList with api

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,26 +1,61 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 import { Wrapper, List, Message, MessageUser, UserImage } from "./styles";
+import { api } from "@services/api";
+
+interface IMessage {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+interface IResponseMessage {
+  data: IMessage[];
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    async function getMessages() {
+      try {
+        const { data: messages }: IResponseMessage = await api.get("messages", {
+          params: {
+            limit: 3
+          }
+        });
+
+        setMessages(messages);
+      } catch {
+        console.log("Failed to get messages");
+      }
+    }
+
+    getMessages();
+  }, []);
+
   return (
     <Wrapper>
       <Image src="/assets/logo.svg" priority={true} alt="DoWhile 2021" width="280" height="28" />
 
       <List>
-        <Message>
-          <p>Não vejo a hora de começar esse DoWhile 2021!</p>
+        {messages.map(message => 
+          <Message key={message.id}>
+            <p>{message.text}</p>
 
-          <MessageUser>
-            <UserImage>
-              <Image src="https://github.com/EddyPBR.png" alt="Edvaldo Junior" width="30" height="30" />
-            </UserImage>
+            <MessageUser>
+              <UserImage>
+                <Image src={message.user.avatar_url} alt={message.user.name} width="30" height="30" />
+              </UserImage>
 
-            <span>
-              Edvaldo Junior
-            </span>
-          </MessageUser>
-        </Message>
+              <span>{message.user.name}</span>
+            </MessageUser>
+          </Message>
+        )}
       </List>
     </Wrapper>
   );
