b5290bf96e226932240b67a7f7ab8052d572578e
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-22 10:25:45 -0300

    feat(mobile): adding status bar on app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,9 @@ import {
   Roboto_700Bold
 } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
-import { theme } from "./src/styles/theme";
+import { StatusBar } from "expo-status-bar";
 
+import { theme } from "./src/styles/theme";
 import { ThemeProvider } from "styled-components";
 
 import { Home } from "./src/screens/Home";
@@ -23,6 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" />
       <Home />
     </ThemeProvider>
   );
