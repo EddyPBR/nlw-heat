218034953db3cb593f392119311271a1a39b91bd
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-22 15:04:14 -0300

    refactor(mobile): adjusts in messages list component styles

--- mobile/src/components/MessagesList/styles.ts
+++ mobile/src/components/MessagesList/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 
 export const ScrollContainer = styled.ScrollView`
+  flex: 1;
+  padding: 0 20px;
 `;
 
 export const ContentContainerStyleObject = {
