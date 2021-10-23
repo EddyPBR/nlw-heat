2711c3b60454dccb9b59f69354445131890f07ff
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-23 10:21:12 -0300

    feat(mobile): adjusts on cmportament in send message form component

--- mobile/src/components/SendMessageForm/index.tsx
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "../Button";
 
@@ -6,15 +6,23 @@ import { Container, Input } from "./styles";
 import { theme } from "../../styles/theme";
 
 export function SendMessageForm() {
+  const [message, setMessage] = useState("");
+  const [sendingMessage, setSendingMessage] = useState(false);
+
   return (
     <Container>
       <Input
         keyboardAppearance="dark"
         placeholder="Qual sua expectativa para o evento?"
         placeholderTextColor={theme.colors.gray1}
+        multiline
+        maxLength={140}
+        onChangeText={setMessage}
+        value={message}
         style={{
           textAlignVertical: "top"
         }}
+        editable={!sendingMessage}
       />
       <Button
         color={theme.colors.white}
