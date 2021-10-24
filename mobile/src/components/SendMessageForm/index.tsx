53b31ea47d093eaadc024c183fa7343e70d68ae9
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-24 14:37:33 -0300

    feat(mobile): integrating send message form with the api

--- mobile/src/components/SendMessageForm/index.tsx
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -5,9 +5,35 @@ import { Button } from "../Button";
 import { Container, Input } from "./styles";
 import { theme } from "../../styles/theme";
 
+import { ToastAndroid, Keyboard } from "react-native";
+
+import { api } from "../../services/api";
+
 export function SendMessageForm() {
   const [message, setMessage] = useState("");
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleCreateMessage() {
+    const messageFormatted = message.trim();
+
+    if(messageFormatted.length > 0) {
+      setSendingMessage(true);
+
+      await api.post("/messages", {
+        message: messageFormatted,
+      });
+  
+      setMessage("");
+  
+      Keyboard.dismiss();
+  
+      ToastAndroid.show("Message created!", ToastAndroid.SHORT);
+
+      setSendingMessage(false);
+    } else {
+      ToastAndroid.show("Write a message to send!", ToastAndroid.SHORT);
+    }
+  }
 
   return (
     <Container>
@@ -28,6 +54,8 @@ export function SendMessageForm() {
         color={theme.colors.white}
         backgroundColor={theme.colors.pink}
         title="ENVIAR MENSAGEM"
+        isLoading={sendingMessage}
+        onPress={handleCreateMessage}
       />
     </Container>
   )
