6f2dc3e2db8467a037e01c7ce01b742efb2cc63b
EddyPBR <edvaldojunior1310@gmail.com>
2021-10-24 14:38:00 -0300

    feat(mobile): integrating component messages list with socket.io

--- mobile/src/components/MessagesList/index.tsx
+++ mobile/src/components/MessagesList/index.tsx
@@ -6,8 +6,30 @@ import { Message, IMessage } from "../Message";
 
 import { api } from "../../services/api";
 
+import io from "socket.io-client";
+
 export function MessagesList() {
   const [messages, setMessages] = useState<IMessage[]>([]);
+
+  const messagesQueue: IMessage[] = [];
+
+  const socket = io(process.env.API_URL || "");
+
+	socket.on("new_message", (newMessage: IMessage) => {
+		messagesQueue.push(newMessage);
+	});
+
+  useEffect(() => {
+		setInterval(() => {
+			if (messagesQueue.length > 0) {
+				setMessages((prevState) =>
+					[messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+				);
+
+				messagesQueue.shift();
+			}
+		}, 3000);
+	}, []);
 
   useEffect(() => {
     async function getMessages() {
